cb710b57022ce6bbc55f9f67524573b70dd98509
Paul laut <paulaut6@gmail.com>
2023-12-11 03:09:37 +0800

    try

--- e2e/testdata/Testdata.ts
+++ e2e/testdata/Testdata.ts
@@ -1,23 +1,26 @@
 
 export type LoginParams = {
+    tcid:string;
     username: string;
     password: string;
 };
 
 export const validCreds: LoginParams[] = [
-    { username: 'student', password: 'Password123' },
+    { tcid:'TC001',username: 'student',
+        password: 'Password123' },
 ];
 
 export const invalidCreds: LoginParams[] = [
-    { username: ' ', password: 'Password123' },
-    { username: 'student', password: ' ' },
-    { username: 'student', password: 'password123' },
-    { username: 'studen2', password: 'Password123' },
-    { username: ' ', password: ' ' }
+    { tcid:'TC002', username: ' ', password: 'Password123' },
+    { tcid:'TC003', username: 'student', password: ' ' },
+    { tcid:'TC004', username: 'student', password: 'password123' },
+    { tcid:'TC005', username: 'studen2', password: 'Password123' },
+    { tcid:'TC006', username: ' ', password: ' ' }
 ];
 
 
 export type RegisterParams = {
+    tcid: string;
     username: string;
     password: string;
 };
